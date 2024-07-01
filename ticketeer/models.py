c847a12d8fdc7449c7c826afd62e1e17db3f252c
benjaminBennewitz <benjamin.bennewitz@gmail.com>
2024-07-02 00:32:51 +0200

    better validation and security

--- ticketeer/models.py
+++ ticketeer/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from kanban_backend import settings
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 class TicketeerTask(models.Model):
     """
@@ -36,13 +37,13 @@ class TicketeerTask(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-    title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=200)
-    content = models.TextField(max_length=500)
+    title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
+    subtitle = models.CharField(max_length=200, validators=[MaxLengthValidator(10)])
+    content = models.TextField(max_length=500, validators=[MinLengthValidator(10)])
     date = models.DateField(default=datetime.date.today)
     prio = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='low')
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='todo')
-    doTime = models.IntegerField(default=0)  # This field stores time in minutes or any other unit you prefer
+    doTime = models.IntegerField(default=0)  # This field stores time in minutes
 
     def __str__(self):
         """
